ce2e4d0d7b9fe1589e76b7674705d6e01179d9fe
super13579 <robert200232003@hotmail.com>
2023-10-25 18:52:11 +0800

    Update
    Update key to access,
    Update access key amount and winners information

--- NBC_luckywheel.py
+++ NBC_luckywheel.py
@@ -4,6 +4,7 @@ import datetime
 import requests
 import json
 import base64
+from streamlit_javascript import st_javascript
 
 def time_difference(timestamp_str):
     # Parse the timestamp string into a datetime object
@@ -29,50 +30,197 @@ NBC_Barear = st.secrets["NBC_Barear"]
 
 
 def get_latest_nbc_tweet():
-	# Endpoint URL
-	url = "https://alpha-api.newbitcoincity.com/api/tweet/list"
+    # Endpoint URL
+    url = "https://alpha-api.newbitcoincity.com/api/tweet/list"
 
-	# Query parameters
-	params = {
-	    "network": "nos",
-	    "version": "2",
-	    "page": "1",
-	    "limit": "500",
-	    "address": "0x58264Ac8e24a101ef90b28616C740863b159083b",
-	    "only_following": "true"
-	}
+    # Query parameters
+    params = {
+        "network": "nos",
+        "version": "2",
+        "page": "1",
+        "limit": "500",
+        "address": "0x58264Ac8e24a101ef90b28616C740863b159083b",
+        "only_following": "true"
+    }
 
-	# Headers
-	headers = {
-	    "Authorization": f"Bearer {NBC_Barear}",
-	}
+    # Headers
+    headers = {
+        "Authorization": f"Bearer {NBC_Barear}",
+    }
 
-	# Making the GET request
-	response = requests.get(url, headers=headers, params=params)
+    # Making the GET request
+    response = requests.get(url, headers=headers, params=params)
 
-	# Parsing the JSON response
-	data = response.json()
-	result_list = []
+    # Parsing the JSON response
+    data = response.json()
+    result_list = []
 
-	for i in data["result"]:
-	    if i["content"] == "Open a Lucky Wheel":
-	        try:
-	            mins, sec = time_difference(i["prize_wheel_event"]["ExpiredAt"])
-	            expir_str = f"{mins} m, {sec} s"
-	        except:
-	            expir_str = "Finished"
-	        entry = {
-	            "img_url": i["prize_wheel_event"]["Twitter"]["TwitterAvatar"],
-	            "user_name": i["prize_wheel_event"]["Twitter"]["TwitterUsername"],
-	            "user_address": i["prize_wheel_event"]["TwitterAddress"],
-	            "expir_date": f"{expir_str}",
-	            "status": i["prize_wheel_event"]["Status"],
-	            "is_holder": i["prize_wheel_event"]["IsHolder"],
-	            "price": i["prize_wheel_event"]["Amount"]
-	        }
-	        result_list.append(entry)
+    def get_access_key_number(wheel_address):
+        url = f"https://alpha-api.newbitcoincity.com/api/player-share/profile?network=nos&address={wheel_address}"
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        return data["result"]["min_holding_requirement"]
 
-	return result_list
+    for i in data["result"]:
+        if i["content"] == "Open a Lucky Wheel":
+            try:
+                mins, sec = time_difference(i["prize_wheel_event"]["ExpiredAt"])
+                expir_str = f"{mins} m, {sec} s"
+            except:
+                expir_str = "Finished"
+            if i["prize_wheel_event"]["Status"] == "running":
+                if i["prize_wheel_event"]["IsHolder"]:
+                    address = i["prize_wheel_event"]["TwitterAddress"]
+                    amount_access = get_access_key_number(address)
+                else:
+                    amount_access = 0
+                winner_avat = ""
+                winner_username = "???"
+                winner_address = "???"
+            else:
+                if i["prize_wheel_event"]["IsHolder"]:
+                    amount_access = i["prize_wheel_event"]["PrizeWheelSpend"]["Token"]["MinHoldingRequirement"]
+                else:
+                    amount_access = 0
+                winner_avat = i["prize_wheel_event"]["PrizeWheelSpend"]["ToTwitter"]['TwitterAvatar']
+                winner_username = i["prize_wheel_event"]["PrizeWheelSpend"]["ToTwitter"]['TwitterUsername']
+                winner_address = i["prize_wheel_event"]["PrizeWheelSpend"]["ToTwitter"]['AddressChecked']
+            entry = {
+                "img_url": i["prize_wheel_event"]["Twitter"]["TwitterAvatar"],
+                "user_name": i["prize_wheel_event"]["Twitter"]["TwitterUsername"],
+                "user_address": i["prize_wheel_event"]["TwitterAddress"],
+                "expir_date": f"{expir_str}",
+                "status": i["prize_wheel_event"]["Status"],
+                "is_holder": amount_access,
+                "price": i["prize_wheel_event"]["Amount"],
+                "winner_url" : winner_avat,
+                "winner_username" : winner_username,
+                "winner_address" : winner_address
+            }
+            result_list.append(entry)
+
+    return result_list
+
+def main_content():
+    col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+    with col1:
+            st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
+        Profile
+    </div>
+    """, unsafe_allow_html=True)
+    with col2:
+            st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
+        Follow
+    </div>
+    """, unsafe_allow_html=True)
+    with col3:
+            st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
+        Price
+    </div>
+    """, unsafe_allow_html=True)
+    with col4:
+            st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
+        Time
+    </div>
+    """, unsafe_allow_html=True)
+    with col5:
+            st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
+        Status
+    </div>
+    """, unsafe_allow_html=True)
+    with col6:
+        st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
+        KeyNeed
+    </div>
+    """, unsafe_allow_html=True)
+
+    with col7:
+        st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
+        Winner
+    </div>
+    """, unsafe_allow_html=True)
+    # if st.button("Start Fetching Data"):
+    with st.empty():
+        while True:
+            new_data = get_latest_nbc_tweet()
+            # st.session_state.data_list.insert(0, new_data)  # insert new data at the top
+                
+            # If data_list exceeds 15 rows, remove the oldest row
+            # if len(st.session_state.data_list) > 15:
+                # st.session_state.data_list.pop(-1)
+            new_data = new_data[:15]
+            # Displaying the data
+            col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+            
+            with col1:
+                for data in new_data:
+                    st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 16px;'>
+        <img src="{data['img_url']}" width="50"/>
+    </div>
+    """, unsafe_allow_html=True)
+            with col2:
+                for data in new_data:
+                    address_url = "https://pro.newbitcoincity.com/alpha/profile/"+data["user_address"]
+                    # st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>{data['expir_date']}</div>", unsafe_allow_html=True)
+                    st.markdown(f"""
+    <div style='text-align: center; margin-bottom: 30px; margin-top: 10px; margin-left: -10px;'>
+        <a href="{address_url}" target="_blank">{data['user_name']}</a>
+    </div>
+    """, unsafe_allow_html=True)
+                    
+                    # st.text(data['status'])
+            with col3:
+                for data in new_data:
+                    st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;white-space: nowrap;'>{data['price']} BTC</div>", unsafe_allow_html=True)
+            with col4:
+                for data in new_data:
+                    st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;white-space: nowrap;'>{data['expir_date']}</div>", unsafe_allow_html=True)
+                    # st.text(data['value'])
+            with col5:
+                for data in new_data:
+                    if data['status'] == "running":
+                        st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: green; color: white;white-space: nowrap;'>{data['status'].upper()}</div>", unsafe_allow_html=True)
+                    else:
+                        st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: red; color: white;white-space: nowrap;'>{data['status'].upper()}</div>", unsafe_allow_html=True)
+                    # st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>{data['status'].upper()}</div>", unsafe_allow_html=True)
+                    # st.text(data['counts'])
+            with col6:
+                for data in new_data:
+                    # is_holder = "Yes" if data['is_holder'] else "No"
+                    if data['is_holder'] != 0:
+                        st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; white-space: nowrap;'>{data['is_holder']} Keys</div>", unsafe_allow_html=True)
+                    else:
+                        st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; white-space: nowrap;  background-color: green; color: white;'> FREE </div>", unsafe_allow_html=True)
+                    # st.text(data['token'])
+            with col7:
+                for data in new_data:
+                    if data['winner_url'] == "":
+                        st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; white-space: nowrap;'> ???? </div>", unsafe_allow_html=True)
+                    else:
+                        winner_url = "https://pro.newbitcoincity.com/alpha/profile/"+data["winner_address"]
+                        st.markdown(f"""
+    <a href="{winner_url}" target="_blank" style="text-decoration: none; color: inherit;">
+        <div style='text-align: center; margin-bottom: -5px;'>
+            <img src="{data['winner_url']}" width="40px"/>
+            <p style="font-size: 10px; margin-bottom: 10px; white-space: nowrap;">{data["winner_username"]}</p>
+        </div>
+    </a>
+    """, unsafe_allow_html=True)
+
+            
+            time.sleep(2)
+
+        # st.write("Produced by 0x0funky: ", "https://twitter.com/0x0funky ")
+        # st.write("FT: ", "https://friend.tech/0x0funky")
+        # st.write("SocialFi Tracker open for free now, will open for key holders only in the future.")
 
 # st.set_page_config(layout="wide")
 st.set_page_config(page_title= "SocialFi-Tracker", page_icon="./SocialFi_Tracker.png")
@@ -101,9 +249,9 @@ st.write('''
         flex-direction: row !important;
     }
     [data-testid="column"] {
-        width: 12% !important;  /* 100% divided by 6 columns */
-        flex: 0 0 12% !important;
-        min-width: 12% !important;
+        width: 10% !important;  /* 100% divided by 6 columns */
+        flex: 0 0 10% !important;
+        min-width: 10% !important;
         font-size: 10px;
     }
     [data-testid="column"] img {
@@ -111,9 +259,29 @@ st.write('''
         height: auto;  /* This ensures the aspect ratio of the image remains unchanged */
         margin-bottom: 10px;
     }
+    [data-testid="column"] p {
+        font-size: 8px;
+        margin-top:-10px;
+        margin-bottom: -10px;
+        margin-left:-5px;
+    }
 }
 </style>
 ''', unsafe_allow_html=True)
+
+ACCESS_KEY = "nbcfunkyonly"
+
+user_input_key = st.text_input("Enter the access key: (Find key in FT room or NBC circle)")
+
+
+if user_input_key == ACCESS_KEY:
+    main_content()
+else:
+    if user_input_key:
+        st.write("Incorrect key! Join 0xFunky FT room or NBC circle to get the key!")
+        st.write("FT:", "https://friend.tech/0x0funky")
+        st.write("NBC:", "https://pro.newbitcoincity.com/alpha/profile/0x58264ac8e24a101ef90b28616c740863b159083b")
+
 
 # custom_css = """
 # <style>
@@ -126,101 +294,4 @@ st.write('''
 # """
 # st.markdown(custom_css, unsafe_allow_html=True)
 # List to store fetched datas
-col1, col2, col3, col4, col5, col6 = st.columns(6)
-with col1:
-		st.markdown(f"""
-<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
-    Profile
-</div>
-""", unsafe_allow_html=True)
-with col2:
-		st.markdown(f"""
-<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
-    Follow
-</div>
-""", unsafe_allow_html=True)
-with col3:
-		st.markdown(f"""
-<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
-    Price
-</div>
-""", unsafe_allow_html=True)
-with col4:
-		st.markdown(f"""
-<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
-    Time
-</div>
-""", unsafe_allow_html=True)
-with col5:
-		st.markdown(f"""
-<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
-    Status
-</div>
-""", unsafe_allow_html=True)
-with col6:
-	st.markdown(f"""
-<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: blue; color: white;'>
-    NeedKey
-</div>
-""", unsafe_allow_html=True)
-# if st.button("Start Fetching Data"):
-with st.empty():
-    while True:
-        new_data = get_latest_nbc_tweet()
-        # st.session_state.data_list.insert(0, new_data)  # insert new data at the top
-            
-        # If data_list exceeds 15 rows, remove the oldest row
-        # if len(st.session_state.data_list) > 15:
-            # st.session_state.data_list.pop(-1)
-        new_data = new_data[:15]
-        # Displaying the data
-        col1, col2, col3, col4, col5, col6 = st.columns(6)
-        
-        with col1:
-            for data in new_data:
-                st.markdown(f"""
-<div style='text-align: center; margin-bottom: 16px;'>
-    <img src="{data['img_url']}" width="50"/>
-</div>
-""", unsafe_allow_html=True)
-        with col2:
-            for data in new_data:
-                address_url = "https://pro.newbitcoincity.com/alpha/profile/"+data["user_address"]
-                # st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>{data['expir_date']}</div>", unsafe_allow_html=True)
-                st.markdown(f"""
-<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>
-    <a href="{address_url}" target="_blank">{data['user_name']}</a>
-</div>
-""", unsafe_allow_html=True)
-        	    
-            	# st.text(data['status'])
-        with col3:
-            for data in new_data:
-                st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>{data['price']} BTC</div>", unsafe_allow_html=True)
-        with col4:
-            for data in new_data:
-        	    st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>{data['expir_date']}</div>", unsafe_allow_html=True)
-            	# st.text(data['value'])
-        with col5:
-            for data in new_data:
-            	if data['status'] == "running":
-            		st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: green; color: white;'>{data['status'].upper()}</div>", unsafe_allow_html=True)
-            	else:
-            		st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: red; color: white;'>{data['status'].upper()}</div>", unsafe_allow_html=True)
-        	    # st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>{data['status'].upper()}</div>", unsafe_allow_html=True)
-            	# st.text(data['counts'])
-        with col6:
-            for data in new_data:
-            	is_holder = "Yes" if data['is_holder'] else "No"
-            	if is_holder == "Yes":
-            		st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px;'>{is_holder}</div>", unsafe_allow_html=True)
-            	else:
-            		st.markdown(f"<div style='text-align: center; margin-bottom: 30px; margin-top: 10px; background-color: green; color: white;'>{is_holder}</div>", unsafe_allow_html=True)
-            	# st.text(data['token'])
 
-        
-        time.sleep(2)
-
-        # st.write("Produced by 0x0funky: ", "https://twitter.com/0x0funky ")
-        # st.write("FT: ", "https://friend.tech/0x0funky")
-        # st.write("SocialFi Tracker open for free now, will open for key holders only in the future.")
