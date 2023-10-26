3d29bbd53f2543f1c09efc0e2b1d8869bab5b978
super13579 <robert200232003@hotmail.com>
2023-10-27 04:55:50 +0800

    Update NBC_luckywheel.py

--- NBC_luckywheel.py
+++ NBC_luckywheel.py
@@ -275,7 +275,7 @@ st.write('''
 </style>
 ''', unsafe_allow_html=True)
 
-ACCESS_KEY = "funkynbcft"
+ACCESS_KEY = "funkylovenbc"
 
 user_input_key = st.text_input("Enter the access key: (Find key in FT room or NBC circle)")
 
